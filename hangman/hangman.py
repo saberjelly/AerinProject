3e4c47be0705fc4b3081134efc4f735252d0f011
saberjelly <skpark@g.clemson.edu>
2022-01-29 15:16:07 -0500

    Added last image of hangman print.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,3 +1,18 @@
 import random
 
 word_list = ["pizza", "apple", "phone", "speakers", "table"]
+hangman_prints = {
+    0: """
+          ______________
+          | /          |
+          |/          ( )
+          |            |
+          |           / \\
+          |
+    """,
+    1: """
+    
+    """
+}
+
+print(hangman_prints[0])
